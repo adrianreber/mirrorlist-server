972171b20345a6b44624024587e23541f835d233
Adrian Reber <adrian@lisas.de>
2020-05-11 10:18:48 +0200

    Added test for metalink_failuredoc()
    Signed-off-by: Adrian Reber <adrian@lisas.de>

--- src/test.rs
+++ src/test.rs
@@ -15,3 +15,22 @@ fn metalink_header_test() {
     assert!(s.contains(&start));
     assert!(s.contains(&end));
 }
+
+#[test]
+fn metalink_failuredoc_test() {
+    let mut start = String::new();
+    let mut end = String::new();
+    let test = String::from("metalink failure test");
+    start.push_str("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n");
+    start.push_str("<metalink version=\"3.0\" xmlns=\"http://www.metalinker.org/\"");
+    start.push_str(" type=\"dynamic\"");
+    start.push_str(" pubdate=\"");
+    end.push_str("\" generator=\"mirrormanager\"");
+    end.push_str(" xmlns:mm0=\"http://fedorahosted.org/mirrormanager\"");
+    end.push_str(">\n");
+    let s = metalink_failuredoc(test.clone());
+    assert!(s.contains(&start));
+    assert!(s.contains(&end));
+    let result = format!("<!-- {}\n-->\n</metalink>\n", &test);
+    assert!(s.contains(&result));
+}
