00d5f34ba79b5132c5e0a54f480d54454f1ff595
Adrian Reber <adrian@lisas.de>
2021-10-16 17:20:19 +0200

    Add database backed tests
    Signed-off-by: Adrian Reber <adrian@lisas.de>

--- src/bin/generate_mirrorlist_cache_test/mod.rs
+++ src/bin/generate_mirrorlist_cache_test/mod.rs
@@ -1,4 +1,5 @@
 use super::*;
+use std::error::Error;
 
 #[test]
 fn get_element_test() {
@@ -18,4 +19,47 @@ fn get_element_test() {
     assert!(!element[0].contains(&String::from("test23")));
     element = get_element(42, &e);
     assert_eq!(0, element.len());
+}
+
+fn get_db_connection() -> Result<PgConnection, Box<dyn Error>> {
+    let database_url = env::var("TEST_DATABASE_URL")?;
+
+    Ok(PgConnection::establish(&database_url)?)
+}
+
+#[test]
+fn get_repositories_test() {
+    let c = match get_db_connection() {
+        Ok(c) => c,
+        Err(e) => {
+            println!("Database connection failed {}", e);
+            panic!();
+        }
+    };
+
+    use db::schema::repository;
+
+    // Clean database
+    assert!(!diesel::delete(repository::dsl::repository)
+        .execute(&c)
+        .is_err());
+
+    let insert = diesel::insert_into(repository::dsl::repository).values((
+        repository::dsl::category_id.eq(4),
+        repository::dsl::version_id.eq(6),
+        repository::dsl::arch_id.eq(8),
+        repository::dsl::directory_id.eq(30001),
+        repository::dsl::prefix.eq("pre".to_string()),
+        repository::dsl::disabled.eq(false),
+    ));
+
+    assert!(!insert.execute(&c).is_err());
+
+    let r = get_repositories(&c);
+    assert_eq!(r[0].0.as_ref().unwrap(), &"pre".to_string());
+    assert_eq!(r[0].1.unwrap(), 4);
+    assert_eq!(r[0].2.unwrap(), 6);
+    assert_eq!(r[0].3.unwrap(), 8);
+    assert_eq!(r[0].4.unwrap(), 30001);
+    assert!(!r[0].5);
 }
